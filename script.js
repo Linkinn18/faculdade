206eb003a06bf517b5252553484f36611e516b93
Fernandinho <xburninging@proton.me>
2025-11-26 07:34:27 -0300

    Adicionando arquivos iniciais do projeto
    aaaa

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    console.log('abriu');
+    var janelaPopup = $("#popup");
+    var fundoPopup = $("#background");
+    var linksPopup = $(".js_popup");
+    var btnFechar = $("#fecharJanela");
+    var conteudoJanela = $("#conteudoJanela");
+    console.log(linksPopup.length);
+    linksPopup.on('click', function(event){
+        event.preventDefault(); //desativa a função do link
+        //abreJanela(janelaPopup, fundoPopup);
+        janelaPopup.toggle();
+        fundoPopup.toggle();
+        var conteudo = $(this).attr("href");
+        console.log("Abrir " + conteudo);
+        var html = $("#conteudo-" + conteudo).html();
+        conteudoJanela.html(html);
+
+    });
+    btnFechar.on('click', function(event){
+        event.preventDefault();
+        janelaPopup.toggle();
+        fundoPopup.toggle();
+    })
+});
